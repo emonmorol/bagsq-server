17b907a5ff07c3bc89a78eed446b7b4c10ca1d41
emonmorol <ekramulhasane69@gmail.com>
2022-05-02 01:47:55 +0600

    corsOption Added

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-// });
 
 function verifyUser(req, res, next) {
   const authorizationToken = req.headers.authorization;
